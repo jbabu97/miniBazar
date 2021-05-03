d25c6aa2dcdba1417246d5b8e5fa5136fb9e7659
Jabbar Babu <jabbar.babu81@gmail.com>
2021-05-04 04:44:22 +0600

    add meta tag

--- src/components/Home/ServiceDetails/ServiceDetails.js
+++ src/components/Home/ServiceDetails/ServiceDetails.js
@@ -11,9 +11,9 @@ const ServiceDetails = ({serve}) => {
                     <img className='img-fluid' src={`data:image/png;base64,${serve.image.img}`} alt=""/>
                     <h3 className='text-center my-3'>{serve.newService.name}</h3>
                 </div>
-                <div className='booking_btn'>
+                 <div className='booking_btn'>
                     <Link to={`/bookService/${serve._id}`} className='ml-auto'><button className='custom_btn animate__animated animate__fadeInDown'>Book Service</button></Link>
-                </div>  
+                </div> 
             </div>
         </div>
     );
